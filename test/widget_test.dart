b4442a276526ccfab0e625db570edfdcd72128f9
Ceyhun Etiz <ceyhun271204@gmail.com>
2026-01-04 22:20:25 +0300

    Add files via upload

---
+++ test/widget_test.dart
@@ -0,0 +1,10 @@
+// test/widget_test.dart
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('App builds successfully', (WidgetTester tester) async {
+    // This is a placeholder test.
+    // We are skipping automated UI tests for now to focus on development.
+    expect(true, isTrue);
+  });
+}
