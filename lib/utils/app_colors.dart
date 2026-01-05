238b03a53c981a8c9fcc37a5d4d434647615c433
Ceyhun Etiz <ceyhun271204@gmail.com>
2026-01-05 23:55:01 +0300

    Delete lib directory

--- lib/utils/app_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primary = Color(0xFFFB923C);
-  static const Color background = Colors.white;
-  static const Color textPrimary = Colors.black;
-  static const Color textSecondary = Colors.black54;
-}
